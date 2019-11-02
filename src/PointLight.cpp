aadc1ba56cf07fc7e9e58398359a6beada413320
Travis Thompson <travis@localhost.localdomain>
2019-11-02 03:00:32 -0400

    compiles on linux, but doesn't run on my laptop

--- src/PointLight.cpp
+++ src/PointLight.cpp
@@ -18,11 +18,11 @@ void PointLight::create()
 auto PointLight::kelvinToRGB(float kelvin) -> glm::vec3
 {
     glm::vec3 color;
-    kelvin = glm::clamp(kelvin, 1000.0F, 40000.0F) / 100.0F;
+    kelvin = glm::clamp(kelvin, 1000.F, 40000.F) / 100.F;
     if (kelvin <= 66.0)
     {
         color.r = 1.0;
-        color.g = glm::clamp(-0.606464F - 0.001742F * (kelvin - 2.0F) + 0.408173F * log(kelvin - 2.0F), 0.0F, 1.0F);
+        color.g = glm::clamp(-0.606464F - 0.001742F * (kelvin - 2.F) + 0.408173F * std::log(kelvin - 2.F), 0.F, 1.F);
         if (kelvin <= 20.0)
         {
             color.b = 0.0;
@@ -30,13 +30,13 @@ auto PointLight::kelvinToRGB(float kelvin) -> glm::vec3
         else
         {
             color.b =
-                glm::clamp(-0.995193F + 0.000323F * (kelvin - 10.0F) + 0.451875F * log(kelvin - 10.0F), 0.0F, 1.0F);
+                glm::clamp(-0.995193F + 0.000323F * (kelvin - 10.F) + 0.451875F * std::log(kelvin - 10.F), 0.F, 1.F);
         }
     }
     else
     {
-        color.r = glm::clamp(1.374910F - 0.001742F * (kelvin - 55.0F) + 0.408173F * log(kelvin - 55.0F), 0.0F, 1.0F);
-        color.g = glm::clamp(1.271287F - 0.0003F * (kelvin - 50.0F) - 0.109708F * log(kelvin - 50.0F), 0.0F, 1.0F);
+        color.r = glm::clamp(1.374910F - 0.001742F * (kelvin - 55.F) + 0.408173F * std::log(kelvin - 55.F), 0.F, 1.F);
+        color.g = glm::clamp(1.271287F - 0.0003F * (kelvin - 50.F) - 0.109708F * std::log(kelvin - 50.0F), 0.F, 1.F);
         color.b = 1.0;
     }
     return color;
